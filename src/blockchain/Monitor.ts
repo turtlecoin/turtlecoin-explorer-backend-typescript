c2f2de5f8cb6fa1fb479e6de476506581d7890f5
ExtraHash <ExtraHash@protonmail.com>
2020-05-01 00:09:16 -0600

    wot

--- src/blockchain/Monitor.ts
+++ src/blockchain/Monitor.ts
@@ -133,6 +133,7 @@ export class Monitor extends EventEmitter {
         if (this.blockStorage.length > 0) {
           await db.sql.transaction(async (trx) => {
             items = this.blockStorage.pop();
+            log.info(items);
             for (const item of items) {
               const block = Block.from(item.block);
               try {
